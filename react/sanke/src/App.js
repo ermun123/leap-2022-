5a3f22919d88070d2b94e0616b732db1f3aadfa6
ermun123 <oyunbayarermun@gmail.com>
2023-01-10 14:09:05 +0800

    smol

--- react/sanke/src/App.js
+++ react/sanke/src/App.js
@@ -8,7 +8,7 @@ const tileWidth = 50;
 const tileHeight = 50;
 const bodyColor = '#00ff00';
 const headColor = '#ff0000';
-const speed = 400;
+const speed = 300;
 
 
 function Tile({ x, y, isHead }) {
@@ -30,30 +30,29 @@ function App() {
   ]);
   const [apple, setApple] = useState({ x: 9, y: 9 });
 
-  const generateApple  = () => {
+  const generateApple = () => {
     let x = random(0, 9);
     let y = random(0, 9);
 
     while (addBody(x, y)) {
-        x = random(0, 9);
-        y = random(0, 9);
+      x = random(0, 9);
+      y = random(0, 9);
     }
 
     setApple({ x: x, y: y });
-};
+  };
 
-const addBody = (x, y) => {
-  for (let i = 0; i < snake.length; i++) {
+  const addBody = (x, y) => {
+    for (let i = 0; i < snake.length; i++) {
       if (snake[i].x === x && snake[i].y === y) return true;
-  }
+    }
+    return false;
+  };
 
-  return false;
-};
-
-const ifEaten = (x, y) => {
-  if (x === apple.x && y === apple.y) return true;
-  return false;
-};
+  const ifEaten = (x, y) => {
+    if (x === apple.x && y === apple.y) return true;
+    return false;
+  };
 
   const random = (max, min) => Math.floor(Math.random() * (max - min + 1)) + min;
 
@@ -75,40 +74,48 @@ const ifEaten = (x, y) => {
         if (ifEaten(item.x, item.y + 1)) {
           generateApple();
           isEaten = true;
-      }
-       if(item.y === yCells - 1 ) return {x: item.x ,y:0};
-       return { x: item.x, y: item.y + 1 };
+        }
+        if (item.y === yCells - 1) return { x: item.x, y: 0 };
+        return { x: item.x, y: item.y + 1 };
       }
       return { x: newSnake[index - 1].x, y: newSnake[index - 1].y };
 
     });
-    setSnake(newSnake);
+    if (isEaten) {
+      setSnake([...newSnake, { x: newSnake[newSnake.length - 1].x, y: newSnake[newSnake.length - 1].y + 1 }]);
+    } else {
+      setSnake(newSnake);
+    }
 
   };
   const moveRight = () => {
+    let isEaten = false;
     let newSnake = [...snake]
     newSnake = newSnake.map((item, index) => {
       if (index === 0) {
-        if (ifEaten(item.x + 1, item.y)) generateApple();
-        if(item.x === xCells - 1 ) return {x:0 ,y:item.y};
+        if (ifEaten(item.x + 1, item.y)) { generateApple(); isEaten = true; }
+        if (item.x === xCells - 1) return { x: 0, y: item.y };
         return { x: item.x + 1, y: item.y };
       }
       return { x: newSnake[index - 1].x, y: newSnake[index - 1].y };
 
     });
-    setSnake(newSnake)
-
+    if (isEaten) {
+      setSnake([...newSnake, { x: newSnake[newSnake.length - 1].x + 1, y: newSnake[newSnake.length - 1].y }]);
+    } else {
+      setSnake(newSnake);
+    }
   };
   const moveUp = () => {
-    let isEaten =false;
+    let isEaten = false;
     let newSnake = [...snake]
     newSnake = newSnake.map((item, index) => {
       if (index === 0) {
         if (ifEaten(item.x, item.y - 1)) {
           generateApple();
           isEaten = true;
-      }
-        if(item.y === 0 ) return {x:0, y:yCells-1 };
+        }
+        if (item.y === 0) return { x: 0, y: yCells - 1 };
         return { x: item.x, y: item.y - 1 };
       }
       return { x: newSnake[index - 1].x, y: newSnake[index - 1].y };
@@ -116,26 +123,31 @@ const ifEaten = (x, y) => {
     });
     if (isEaten) {
       setSnake([...newSnake, { x: newSnake[newSnake.length - 1].x, y: newSnake[newSnake.length - 1].y + 1 }]);
-  } else {
+    } else {
       setSnake(newSnake);
-  }
+    }
 
   };
   const moveLeft = () => {
+    let isEaten = false;
     let newSnake = [...snake]
     newSnake = newSnake.map((item, index) => {
       if (index === 0) {
         if (ifEaten(item.x, item.y - 1)) {
           generateApple();
-          
-      }
-        if(item.x === 0 ) return {x:xCells-1 ,y:item.y};
+          isEaten = true
+        }
+        if (item.x === 0) return { x: xCells - 1, y: item.y };
         return { x: item.x - 1, y: item.y };
       }
       return { x: newSnake[index - 1].x, y: newSnake[index - 1].y };
 
     });
-    setSnake(newSnake)
+    if (isEaten) {
+      setSnake([...newSnake, { x: newSnake[newSnake.length - 1].x, y: newSnake[newSnake.length - 1].y + 1 }]);
+    } else {
+      setSnake(newSnake);
+    }
 
   };
 
@@ -185,13 +197,13 @@ const ifEaten = (x, y) => {
 
   return (
     <div className='wrapper' onKeyDown={handleKeyDown} tabIndex={0}>
-      <h1 style={{backgroundColor:'coral', width:'280px', height:'40px', textAlign:'center', borderRadius:'50px'}}><span>🐍</span>SNAKE GAME</h1>
+      <h1 style={{ backgroundColor: 'coral', width: '280px', height: '40px', textAlign: 'center', borderRadius: '50px' }}><span>🐍</span>SNAKE GAME</h1>
       <div className='board' style={{ width: xCells * tileWidth, height: yCells * tileHeight, }}>
         {snake.map((item, index) => {
           const isHead = index === 0;
           return <Tile x={item.x} y={item.y} isHead={isHead} key={`snake-tile-${index}`} />;
         })}
-        <div style={{ width: tileWidth, height: tileHeight, position: 'absolute',top: apple.y * tileWidth,left: apple.x * tileHeight,background: 'orange',    borderRadius:'50px'}}></div>
+        <div style={{ width: tileWidth, height: tileHeight, position: 'absolute', top: apple.y * tileWidth, left: apple.x * tileHeight, background: 'orange', borderRadius: '50px' }}></div>
       </div>
     </div>
   );
